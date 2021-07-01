4aa4131da60c5e1ffa47d95b5e7e694008ee5272
kostya <kish@campus.technion.ac.il>
2021-07-01 09:43:35 +0000

    deleted build folder and added gitignoe

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/hw4/wet
-# Build directory: /home/student/hw4/wet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
