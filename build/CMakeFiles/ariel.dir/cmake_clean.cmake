4df3a5a1dd3837026e8f488ff69deb57bbf27d81
student <yosefisraelgoren@gmail.com>
2021-07-01 09:25:27 +0000

    continued testing

---
+++ build/CMakeFiles/ariel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ariel.dir/os_hw4_part3_tests-main/test.cpp.o"
+  "ariel"
+  "ariel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ariel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/test_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_3.dir/test_3.cpp.o"
+  "test_3"
+  "test_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
